213a8704de99a22d8c03b4d16d8e0d3af5e10427
Marek Waszkiewicz <mwaszkiewicz@anaconda.com>
2023-11-19 23:45:04 +0100

    try to build on windows

---
+++ recipe/bld.bat
@@ -0,0 +1,6 @@
+mkdir %LIBRARY_INC%
+copy "%SRC_DIR%\sse2neon.h" "%LIBRARY_INC%\sse2neon.h"
+
+rem Makefile not compatible with nmake
+rem nmake check
+
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,5 @@
 mkdir ${PREFIX}/include
 cp ${SRC_DIR}/sse2neon.h ${PREFIX}/include
 
-${CC} --version
-${CXX} --version
-
-
 make check
 
