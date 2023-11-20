5419bfd7e4d834e8dd190bb9f09798f202a350ee
Marek Waszkiewicz <mwaszkiewicz@anaconda.com>
2023-11-20 09:03:08 +0100

    try to fix macos build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-mkdir ${PREFIX}/include
+if [ -d ${PREFIX}/include ];
+then
+    echo "Directory ${PREFIX}/include exists"
+else
+    echo "Error: Directory does not exists"
+    mkdir ${PREFIX}/include
+fi
+
 cp ${SRC_DIR}/sse2neon.h ${PREFIX}/include
 
 make check
